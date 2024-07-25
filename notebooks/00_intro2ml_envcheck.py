8c6e23cfdc83b8fba93aff2bc7104b5988989cec
Peter Steinbach <p.steinbach@hzdr.de>
2024-07-25 17:35:12 +0200

    env check notebook added

---
+++ notebooks/00_intro2ml_envcheck.py
@@ -0,0 +1,38 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.3
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Environment Check
+#
+# This notebook is intended for all learners to check their environment. Click through it to see if your environment is setup correctly.
+
+# %%
+import torch
+
+print("torch installed, ",torch.__version__)
+
+# %%
+import torchvision
+
+print("torchvision installed, ",torchvision.__version__)
+
+# %%
+
+import seaborn
+
+print("seaborn installed, ",seaborn.__version__)
+
+# %% [markdown]
+# If you were able to run the notebook until this point without any errors, you are
+# READY to GO!
