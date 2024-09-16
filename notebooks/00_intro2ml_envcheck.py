c72b9accddce0717b0dda5d93294be80386f1880
Peter Steinbach <p.steinbach@hzdr.de>
2024-09-16 22:09:40 +0200

    added check for mnist1d

--- notebooks/00_intro2ml_envcheck.py
+++ notebooks/00_intro2ml_envcheck.py
@@ -29,7 +29,33 @@ import seaborn
 print("seaborn installed, ", seaborn.__version__)
 
 # %% [markdown]
-# If you were able to run the notebook until this point without any errors, you are
+r"""
+For most of the notebooks, we require the handy [MNIST1D](https://github.com/greydanus/mnist1d) dataset which is both small, versatile and tricky. The dataset can be installed as a `pip` package.
+
+If you are working on CERN's SWAN, you need to install this atop of the LCG `105a` image. To do so, create a new cell below and run the following command in it:
+```
+!python -m pip install --user mnist1d
+```
+This will install `mnist1d` in your CERNBox environment.
+"""
+
+# %%
+import mnist1d
+
+if hasattr(mnist1d, "get_dataset"):
+    print("mnist1d installed!")
+else:
+    print("mnist1d NOT installed. See instructions for details.")
+
+# %% [markdown]
+r"""
+If you were able to run the notebook until this point without any errors, you are
+"""
 
 # %%
 print("READY to GO!")
+
+# %% [markdown]
+r"""
+If you are working on CERN's SWAN system, please log out and close all notebooks. Then login again, but make sure to tick `Use Python Packages installed on CERNBox` in the __Software Stack__ pane.
+"""
